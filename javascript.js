ccadab0b844e98907ed0c418d60c84554dfa785e
eclecticexistential <allofitbra@gmail.com>
2017-09-22 01:10:46 -0700

    Added slide right and buttons. Need to use flexbox and dynamic images to create zoom effect instead of appending. May make it easier to put image back in place after it returns to original state.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,38 @@
-var click = ('ontouchstart' in document.documentElement)  ? 'touchstart' : 'mousedown';
+var $mainImages = $(".main-page-images");
+var $doc = $('<div id="overlay"><input type="button" class="navButtons" id="toLeft" value="<"><input type="button" class="navButtons" id="toRight" value=">"/><input type="button" class="navButtons" id="toExit" value="X"/></div>');
+var $secondMainPage = $("#second-main-page");
 
-$(".images").on(click,function(){
-	var $mainImages = $(".main-page-images");
-	var $doc = $('<div id="overlay"></div>');
+
+$(".images").on("click",function(){
 	$doc.append($(this));
 	$mainImages.append($doc);
-	$mainImages.delay(400).animate({"margin-right":'+=600'},2000,"linear",function(){
-		$(this).after();
+	$mainImages.delay(400).animate({"margin-right":'+=600'},1000,"linear");
+	$(this).unbind("click");
+		$secondMainPage.animate({"margin-left":'+=760'},500,"linear", function(){
+		$secondMainPage.css("visibility",'visible');
 	});
-})
+});
+
+	$mainImages.on("click", '#toExit', function(){
+		$mainImages.delay(400).animate({"margin-right":'-=600'},1000,"linear");
+		$secondMainPage.css("z-index",'-1').delay(700).animate({"margin-right":'-=760'},500,"linear", function(){
+		$secondMainPage.delay(400).css("visibility","hidden");
+	});
+		$mainImages.unbind();
+		$doc.hide();
+	});
+
+
+
+$mainImages.on("click", '#toLeft', function(){
+	alert("Hi");
+});
+
+$mainImages.on("click", '#toRight', function(){
+	alert("Hi");
+});
+
+
 
 //image onclick should get bigger
 	//image should move to the left
