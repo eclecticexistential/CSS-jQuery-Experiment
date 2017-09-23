be2144ff303dcf7390d9ea3c0facd8d86f743744
eclecticexistential <allofitbra@gmail.com>
2017-09-23 02:43:22 -0700

    Got carousel, main images, and appended buttons all linked up. Site has a few bugs, such as the overlay randomly growing shorter or the carousel not moving certain images. Was able to generate text, make it invisible, and append it to the right side. Need to work on finding an efficient way to toggleClass.

--- javascript.js
+++ javascript.js
@@ -2,26 +2,36 @@ var $mainImages = $(".main-page-images");
 var $doc = $('<div id="overlay"></div>');
 var $navButtons = $('<input type="button" class="navButtons" id="toLeft" value="<"><input type="button" class="navButtons" id="toRight" value=">"/><input type="button" class="navButtons" id="toExit" value="X"/>');
 var $secondMainPage = $("#second-main-page");
-var $old_html = $mainImages.html();
-var $images = $(".images");
 var $navForImages = $("#navForImages");
+var $old_html = $mainImages.html();
+var $old_nav = $navForImages.html();
+var $images = $(".images");
 var $array=[];
+var $arrayImages=$('<img class="navimages" src="http://placeimg.com/100/100/arch" alt="architecture"><img class="navimages" src="http://placeimg.com/100/100/people" alt="people"><img class="navimages" src="http://placeimg.com/100/100/tech" alt="tech"><img class="navimages" src="http://placeimg.com/100/100/arch/grayscale" alt="arch grayscale"><img class="navimages" src="http://placeimg.com/100/100/people/sepia" alt="people sepia"><img class="navimages" src="http://placeimg.com/100/100/arch/sepia" alt="arch sepia">');
+var $navImages = $(".navimages");
+var $actArrImg = [];
+var $allOf = $(".here>div");
 
+//add text block on second page//
+
+function makeVisible(){
+		$allOf.first().addClass("selected");
+		$secondMainPage.append($(".selected"));
+}
 
 
 $images.on("click",imagesMove);
 
 function imagesMove(){
-	var $this = $(this);
-	if($images!=$this){
-		fillArray()
-	}
+	fillArray();
+	makeVisible()
+	$navForImages.append($actArrImg);
 	//makes it easier to add/remove buttons and images from overlay//
 	$doc.append($navButtons);
-	$doc.append($this);
+	$doc.append($(this));
 	$mainImages.append($doc);
 	$mainImages.animate({"margin-right":'+=600'},1000,"linear");
-	$(this).unbind("click");
+	$doc.children("img").unbind("click");
 };
 
 	
@@ -43,28 +53,68 @@ function catchAll(){
 //fills array with images that are not $(this)//
 
 function fillArray(){
-	var counter = 1;
-	while(counter<$images.length){
-		$array.push($images.addClass("navimages"));
-		$navForImages.append($array);
-		counter++
+	for(i=0;i<$images.length;i++){
+		$array.push($images[i]);
+	}
+	for(i=0;i<$arrayImages.length;i++){
+		$actArrImg.push($arrayImages[i]);
 	}
 }
 
+//alters image on overlay//
+
+function changeStyle(n){
+	var x = $doc.children("img").attr("alt");
+	var y;
+	for(i=0;i<$array.length;i++){
+		if($($array[i]).attr('alt')==x){
+			y=i;
+		}
+	}
+	var z = y+n;
+	if(z>$array.length-1){z=0};
+	if(z<0){z=$array.length-1};
+	$doc.children($images).remove();
+	$doc.append($navButtons);
+	$doc.append($array[z])
+	$doc.children("img").unbind("click");
+}
+
+
+//alter text blocks//
+
+function updateVisible(){
+		if($allOf.first().hasClass("selected")){
+			$allOf.first().removeClass("selected");
+			}
+	}
+
+
+//alters mini icon array to move images//
+
+function changeMain(m){
+	if(m<0){
+	var first = $actArrImg.shift();
+	$actArrImg.push(first);
+	$navForImages.append($actArrImg);
+	}
+	if(m>0){
+	var last=$actArrImg.pop();
+	$actArrImg.unshift(last);
+	$navForImages.append($actArrImg);
+	}
+	updateVisible();
+}
+
+
+//overlay buttons to move images on both sides //
+
 $mainImages.on("click", '#toLeft', function(){
-	alert("Hi");
+	changeStyle(-1);
+	changeMain(-1);
 });
 
 $mainImages.on("click", '#toRight', function(){
-	alert("Hi");
+	changeStyle(1);
+	changeMain(1);
 });
-
-
-
-//image onclick should get bigger
-	//image should move to the left
-		//nav buttons should appear on enlarged image
-			//other images should move to small circles on right
-				//content about person should appear below small circles
-					//images should slide with nav
-						//bio should slide as well
