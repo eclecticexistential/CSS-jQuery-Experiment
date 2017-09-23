88584d4de0fef5a2d3bcea7b5e5276dc59e7912f
eclecticexistential <allofitbra@gmail.com>
2017-09-22 17:20:41 -0700

    Prepared images for carousel. Having issues with using this multiple times in a function. loadArray uses this to populate a map that doesn't have the img pressed in the line up, whereas imageMove uses this to append the clicked image to the overlay. Both functions work by themselves, however, they do not work at the same time.

--- javascript.js
+++ javascript.js
@@ -2,39 +2,54 @@ var $mainImages = $(".main-page-images");
 var $doc = $('<div id="overlay"></div>');
 var $navButtons = $('<input type="button" class="navButtons" id="toLeft" value="<"><input type="button" class="navButtons" id="toRight" value=">"/><input type="button" class="navButtons" id="toExit" value="X"/>');
 var $secondMainPage = $("#second-main-page");
-var old_html = $mainImages.html();
+var $old_html = $mainImages.html();
+var $images = $(".images");
+var $navForImages = $("#navForImages");
+var $array=[];
 
 
 
-$(".images").on("click", imagesMove);
+$images.on("click",imagesMove);
 
 function imagesMove(){
+	var $this = $(this);
+	if($images!=$this){
+		fillArray()
+	}
+	//makes it easier to add/remove buttons and images from overlay//
 	$doc.append($navButtons);
-	$doc.append($(this));
+	$doc.append($this);
 	$mainImages.append($doc);
-	$mainImages.delay(400).animate({"margin-right":'+=600'},1000,"linear");
+	$mainImages.animate({"margin-right":'+=600'},1000,"linear");
 	$(this).unbind("click");
-		$secondMainPage.animate({"margin-left":'+=760'},500,"linear", function(){
-		$secondMainPage.css("visibility",'visible');
-	});
 };
+
 	
 $mainImages.on("click", '#toExit', exitButton);
 
 function exitButton(){
 		$mainImages.delay(400).animate({"margin-right":'-=600'},1000,"linear");
-		$secondMainPage.css("z-index",'-1').delay(1000).animate({"margin-left":'-=1'},1000,"linear", function(){
-		$secondMainPage.animate({"margin-left":'-=760'},1000,"linear").css("visibility","hidden");
-	});
-	catchAll();
+		catchAll();
 	};
+
 	
+//removes items from overlay and resets main-page-images//
 function catchAll(){
 		$doc.empty();
-		$mainImages.html(old_html);
+		$mainImages.html($old_html);
 		$(".images").on("click", imagesMove);
 };
 
+//fills array with images that are not $(this)//
+
+function fillArray(){
+	var counter = 1;
+	while(counter<$images.length){
+		$array.push($images.addClass("navimages"));
+		$navForImages.append($array);
+		counter++
+	}
+}
 
 $mainImages.on("click", '#toLeft', function(){
 	alert("Hi");
