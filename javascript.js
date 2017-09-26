99d4db2c6501eac71fc91068fd06acc81a6fd038
eclecticexistential <allofitbra@gmail.com>
2017-09-25 18:27:49 -0700

    Text blocks and main images move together. Half of the icon images alter in sync and more testing will be needed. Solution for overlay bug remains undiscovered.

--- javascript.js
+++ javascript.js
@@ -7,28 +7,80 @@ var $old_html = $mainImages.html();
 var $old_nav = $navForImages.html();
 var $images = $(".images");
 var $array=[];
-var $arrayImages=$('<img class="navimages" src="http://placeimg.com/100/100/arch" alt="architecture"><img class="navimages" src="http://placeimg.com/100/100/people" alt="people"><img class="navimages" src="http://placeimg.com/100/100/tech" alt="tech"><img class="navimages" src="http://placeimg.com/100/100/arch/grayscale" alt="arch grayscale"><img class="navimages" src="http://placeimg.com/100/100/people/sepia" alt="people sepia"><img class="navimages" src="http://placeimg.com/100/100/arch/sepia" alt="arch sepia">');
+//var $arrayImagesExtra=$('<img class="navimages" src="http://placeimg.com/100/100/arch" alt="architecture"><img class="navimages" src="http://placeimg.com/100/100/people" alt="people"><img class="navimages" src="http://placeimg.com/100/100/tech" alt="tech"><img class="navimages" src="http://placeimg.com/100/100/arch/grayscale" alt="arch grayscale"><img class="navimages" src="http://placeimg.com/100/100/people/sepia" alt="people sepia"><img class="navimages" src="http://placeimg.com/100/100/arch/sepia" alt="arch sepia">');
 var $navImages = $(".navimages");
 var $actArrImg = [];
 var $allOf = $(".here>div");
+var indexOf;
+var $arrayImages=$('<img class="navimages" id="0" src="EmiliaClarke.png" alt="Emilia Clarke"><img class="navimages" id="1" src="AllisonRandal.png" alt="Allison Randal"><img class="navimages" id="2" src="DanaiGurira.png" alt="Danai Gurira"><img class="navimages" id="3" src="RosarioDawson.png" alt="Rosario Dawson"><img class="navimages" id="4" src="LeftEye.png" alt="Left Eye - TLC"><img class="navimages" id="5" src="AnnMargaret.png" alt="Ann Margaret"><img class="navimages" id="6" src="GraceHopper.png" alt="Grace Hopper"><img class="navimages" id="7" src="YoaniSanchez.png" alt="Yoani Sanchez">');
+
+
+//fills array with images//
+
+function fillArray(){
+	for(i=0;i<$images.length;i++){
+		$array.push($images[i]);
+	}
+	for(i=0;i<$arrayImages.length;i++){
+		$actArrImg.push($arrayImages[i]);
+	}
+}
+
+
+fillArray();
+
 
 //add text block on second page//
 
 function makeVisible(){
-		$allOf.first().addClass("selected");
-		$secondMainPage.append($(".selected"));
+		if(event.target.tagName=="IMG"){
+			indexOf = $.inArray(event.target,$images);
+					putIn2nd(indexOf);
+				}
+			}
+
+// needed for second click through cycle on main page otherwise event.target doesn't match//
+function willItBlend(){
+		if(event.target.tagName=="IMG"){
+			for(i=0;i<$images.length;i++){
+			if(event.target.id==i){
+				indexOf=i;
+				putIn2nd(indexOf);
+				}
+			}
+		}
 }
 
 
-$images.on("click",imagesMove);
+function putIn2nd(){
+	if($allOf.hasClass("selected")){
+		$allOf.removeClass("selected");
+			$(".here").append($allOf);
+		}
+		if($.inArray(indexOf,$allOf)){
+				if(!$($allOf[indexOf]).hasClass("selected")){
+					$secondMainPage.append($($allOf[indexOf]).addClass("selected"));
+				} 
+			}
+		}
+
+//make the $doc image disappear from navimage array on first click//
+
+function removeNavImage(){
+	if(event.target.tagName=="IMG"){
+			indexOf = $.inArray(event.target,$images);
+			$($actArrImg[indexOf]).hide();
+	}
+}
+
+$images.on("click",imagesMove).on("click",makeVisible);
 
 function imagesMove(){
-	fillArray();
-	makeVisible()
 	$navForImages.append($actArrImg);
 	//makes it easier to add/remove buttons and images from overlay//
 	$doc.append($navButtons);
 	$doc.append($(this));
+	removeNavImage(event.target);
 	$mainImages.append($doc);
 	$mainImages.animate({"margin-right":'+=600'},1000,"linear");
 	$doc.children("img").unbind("click");
@@ -47,19 +99,10 @@ function exitButton(){
 function catchAll(){
 		$doc.empty();
 		$mainImages.html($old_html);
-		$(".images").on("click", imagesMove);
+		$(".images").on("click", imagesMove).on("click",willItBlend);
 };
 
-//fills array with images that are not $(this)//
 
-function fillArray(){
-	for(i=0;i<$images.length;i++){
-		$array.push($images[i]);
-	}
-	for(i=0;i<$arrayImages.length;i++){
-		$actArrImg.push($arrayImages[i]);
-	}
-}
 
 //alters image on overlay//
 
@@ -74,37 +117,46 @@ function changeStyle(n){
 	var z = y+n;
 	if(z>$array.length-1){z=0};
 	if(z<0){z=$array.length-1};
-	$doc.children($images).remove();
-	$doc.append($navButtons);
-	$doc.append($array[z])
-	$doc.children("img").unbind("click");
+		$doc.children($images).remove();
+		$doc.append($navButtons);
+		$doc.append($array[z])
+		$doc.children("img").unbind("click");
 }
 
+//change text block with left and right buttons//
 
-//alter text blocks//
-
-function updateVisible(){
-		if($allOf.first().hasClass("selected")){
-			$allOf.first().removeClass("selected");
+function updateTextByBtn(m){
+	for(i=0;i<$allOf.length;i++){
+			if($($allOf[i]).hasClass("selected")){
+				var $addNewPos = i+m;
+				if($addNewPos>$allOf.length-1){
+					$addNewPos=0;
+				}
+				if($addNewPos<0){
+					$addNewPos=$allOf.length-1;
+				}
+					$(".here").append($allOf[i]);
+					$($allOf[i]).removeClass("selected");
+					$secondMainPage.append($($allOf[$addNewPos]).addClass("selected"));
+				break;
 			}
-	}
+		}
+}
 
 
 //alters mini icon array to move images//
 
 function changeMain(m){
-	if(m<0){
-	var first = $actArrImg.shift();
-	$actArrImg.push(first);
-	$navForImages.append($actArrImg);
-	}
-	if(m>0){
-	var last=$actArrImg.pop();
-	$actArrImg.unshift(last);
-	$navForImages.append($actArrImg);
-	}
-	updateVisible();
+	var x = parseInt($($actArrImg).not(":visible").attr("id"));
+	$($actArrImg[x]).show();
+	console.log(x);
+	var y = x+m;
+	if(y>$actArrImg.length-1){y=0;}
+	if(y<0){y=$actArrImg.length-1;}
+	console.log(y)
+	$($actArrImg[y]).hide();
 }
+
 
 
 //overlay buttons to move images on both sides //
@@ -112,9 +164,11 @@ function changeMain(m){
 $mainImages.on("click", '#toLeft', function(){
 	changeStyle(-1);
 	changeMain(-1);
+	updateTextByBtn(-1);
 });
 
 $mainImages.on("click", '#toRight', function(){
 	changeStyle(1);
 	changeMain(1);
+	updateTextByBtn(1);
 });
